312e6dfb321ae6616e9825c36a517b24c8748ffd
alex baboy <alexbaboy2016@gmail.com>
2021-08-24 16:16:12 +0300

    tsconfig

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,33 @@ class Person {
 }
 
 const al = new Person('Alex')
+
+// demo comment
+const btn: Element = document.querySelector('#btn')!
+
+if(btn)
+    btn.addEventListener('click', ()=> {
+        console.log('Btn clicked')
+    })
+
+// ==============
+
+let anyFlag
+
+const globalVar = 'message'
+
+function logInfo(data: string, _?: number) {
+    //const message = 'string'
+    console.log(data)
+    anyFlag = true
+    console.log(anyFlag)
+}
+
+logInfo('log string')
+
+function multiple(a: number, b: number) {
+    if(a && b)
+        return a*b
+
+    return
+}
